7fb32950a6fb04e2626b6014ee1fff0451b0fc11
Rian Furtado <rianoliveirafurtado@gmail.com>
2022-04-15 22:42:41 -0300

    Implementação das classes Board e Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,16 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Rian-Futado
+		// TODO Rian-Furtado
 		
 		
-		Position pos  = new Position(3,5);
-		System.out.println(pos);
+		Board board = new Board(8,8);
+		
 
 	}
 
